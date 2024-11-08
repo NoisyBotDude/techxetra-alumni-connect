e4c09ad9a7dbe913547e7ce3e40736807c796026
NoisyBotDude <ashishsarmah11@gmail.com>
2024-11-08 13:08:02 +0530

    Update firebase configuration

--- src/lib/firebase-app.js
+++ src/lib/firebase-app.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDRKndPUu1J1g_RIBPdPiX5wjRF8tt_ou0",
+  authDomain: "techxetra-alumni-connect.firebaseapp.com",
+  projectId: "techxetra-alumni-connect",
+  storageBucket: "techxetra-alumni-connect.firebasestorage.app",
+  messagingSenderId: "913032497726",
+  appId: "1:913032497726:web:ee9095a381dac7de3f9f6f",
+  measurementId: "G-Q1GYKVE3BX"
 };
 
 const app = initializeApp(firebaseConfig);
